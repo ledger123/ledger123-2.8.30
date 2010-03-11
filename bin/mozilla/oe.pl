a360a6bdc49f7489ca9563963f1e19fd9c06b24f
Armaghan Saqib <ar.saqib@gmail.com>
2010-03-11 10:44:51 +0500

    Added ship all to shipping transfer/receive

--- bin/mozilla/oe.pl
+++ bin/mozilla/oe.pl
@@ -2652,6 +2652,7 @@ sub display_ship_receive {
 
   %button = ('Update' => { ndx => 1, key => 'U', value => $locale->text('Update') },
              'Print' => { ndx => 2, key => 'P', value => $locale->text('Print') },
+	     'Ship all' => { ndx => 5, key => 'A', value => $locale->text('Ship all') },
 	     'Ship to' => { ndx => 4, key => 'T', value => $locale->text('Ship to') },
 	     'E-mail' => { ndx => 5, key => 'E', value => $locale->text('E-mail') },
 	     'Done' => { ndx => 11, key => 'D', value => $locale->text('Done') },
@@ -2660,7 +2661,7 @@ sub display_ship_receive {
   for ("Update", "Print") { $form->print_button(\%button, $_) }
   
   if ($form->{type} eq 'ship_order') {
-    for ('Ship to', 'E-mail') { $form->print_button(\%button, $_) }
+    for ('Ship all', 'Ship to', 'E-mail') { $form->print_button(\%button, $_) }
   }
   
   $form->print_button(\%button, 'Done');
