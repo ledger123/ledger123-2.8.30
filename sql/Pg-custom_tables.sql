fc8963f6ed9ab79a071d0c0aecd516218f2ec60a
Armaghan Saqib <ar.saqib@gmail.com>
2010-06-25 01:52:10 -0400

    Tables for use by ledgercart. Run the following statements using psql or phppgadmin or pgadmin3
    CREATE SEQUENCE customerloginid start 1;
    SELECT nextval('customerloginid');
    
    CREATE TABLE customercart (
        cart_id character varying(32),
        customer_id integer,
        parts_id integer,
        qty double precision DEFAULT 0,
        price double precision DEFAULT 0,
        taxaccounts text
    );
    
    CREATE TABLE customerlogin (
        id integer DEFAULT nextval('customerloginid') primary key,
        "login" character varying(100) unique,
        passwd character(32),
        "session" character(32) unique,
        session_exp character varying(20),
        customer_id integer
    );
    
    CREATE TABLE partsattr (
        parts_id INTEGER,
        hotnew VARCHAR(3)
    );
    
    	modified:   sql/Pg-custom_tables.sql

--- sql/Pg-custom_tables.sql
+++ sql/Pg-custom_tables.sql
@@ -101,3 +101,31 @@ WHERE trans_id IN (SELECT id FROM ap);
 CREATE INDEX fifo_trans_id ON fifo (trans_id);
 CREATE INDEX invoice_qty ON invoice (qty);
 
+-- Tables for Ledgercart
+CREATE SEQUENCE customerloginid start 1;
+SELECT nextval('customerloginid');
+
+CREATE TABLE customercart (
+    cart_id character varying(32),
+    customer_id integer,
+    parts_id integer,
+    qty double precision DEFAULT 0,
+    price double precision DEFAULT 0,
+    taxaccounts text
+);
+
+CREATE TABLE customerlogin (
+    id integer DEFAULT nextval('customerloginid') primary key,
+    "login" character varying(100) unique,
+    passwd character(32),
+    "session" character(32) unique,
+    session_exp character varying(20),
+    customer_id integer
+);
+
+CREATE TABLE partsattr (
+    parts_id INTEGER,
+    hotnew VARCHAR(3)
+);
+
+
