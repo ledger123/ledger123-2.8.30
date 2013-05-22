ffae32d220df18c6b56cab43cee1e5e0ebaddba9
Armaghan Saqib <ar.saqib@gmail.com>
2013-05-22 07:21:38 +0500

    Fixed Bug #14387 (custom branch) 	modified:   SL/IS.pm

--- SL/IS.pm
+++ SL/IS.pm
@@ -51,19 +51,6 @@ sub invoice_details {
  
   my %translations;
 
-  my %taxaccs;
-  # for managing 0% taxes for runmyaccounts
-  for my $tax1 (split / /, $form->{taxaccounts}){
-    for my $i (1 .. $form->{rowcount} - 1) {
-      for my $tax2 (split / /, $form->{"taxaccounts_$i"}){
-   	if ($tax1 eq $tax2){
-	   $taxaccs{$tax2} = 1 if !$taxaccs{$tax2};
-	}
-      }
-    }
-  }
-  @taxaccounts = keys %taxaccs;
-
   $query = qq|SELECT p.description, t.description
               FROM project p
 	      LEFT JOIN translation t ON (t.trans_id = p.id AND t.language_code = '$form->{language_code}')
@@ -283,6 +270,8 @@ sub invoice_details {
       $form->{"linetotal_$i"} = $form->format_amount($myconfig, $linetotal, $form->{precision}, "0");
       push(@{ $form->{linetotal} }, $form->{"linetotal_$i"});
       
+      @taxaccounts = split / /, $form->{"taxaccounts_$i"};
+
       my $ml = 1;
       my @taxrates = ();
       
@@ -311,7 +300,6 @@ sub invoice_details {
 	    }
 	  }
 	}
-
 	if ($form->{taxincluded}) {
 	  $tax += $linetotal * ($taxrate / (1 + ($taxrate * $ml)));
 	} else {
@@ -331,6 +319,7 @@ sub invoice_details {
       }
 
     }
+
 
     # add subtotal
     if ($form->{groupprojectnumber} || $form->{grouppartsgroup}) {
@@ -387,6 +376,7 @@ sub invoice_details {
 
   $tax = 0;
   $taxrate = 0;
+
   for (sort keys %taxaccounts) {
     #if ($taxaccounts{$_} = $form->round_amount($taxaccounts{$_}, $form->{precision})) {
       $tax += $taxaccounts{$_};
