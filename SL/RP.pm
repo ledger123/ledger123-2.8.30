e1ce38b282e47f1862de3f4f31d92f8e075f40ef
Armaghan Saqib <ar.saqib@gmail.com>
2013-08-20 11:05:12 +0500

    Added drill down to gifi accounts balances list from income statement for gifi accounts. 	modified:   SL/RP.pm

--- SL/RP.pm
+++ SL/RP.pm
@@ -173,14 +173,19 @@ sub income_statement {
   
   my $str;
   my $tmpstr;
-  my $translink = qq|ca.pl?action=list_transactions|;
-  for (qw(path login fromdate todate method)){ $translink .= qq|&$_=$form->{$_}| }
-  for (qw(projectnumber department)){ 
-     $translink .= "&$_=" . $form->escape($form->{$_});
+  my $translink;
+
+  if ($form->{accounttype} eq 'gifi'){
+    $translink = qq|rp.pl?action=continue&accounttype=standard&nextsub=generate_trial_balance|;
+    for (qw(path login fromdate todate method)){ $translink .= qq|&$_=$form->{$_}| }
+    for (qw(projectnumber department)){ $translink .= "&$_=" . $form->escape($form->{$_}) }
+  } else {
+    $translink = qq|ca.pl?action=list_transactions|;
+    for (qw(path login fromdate todate method)){ $translink .= qq|&$_=$form->{$_}| }
+    for (qw(projectnumber department)){ $translink .= "&$_=" . $form->escape($form->{$_}) }
   }
-  
+ 
   foreach $category (@categories) {
-    
     foreach $key (sort keys %{ $form->{$category} }) {
       # push description onto array
       
@@ -188,7 +193,11 @@ sub income_statement {
       
       if ($form->{$category}{$key}{charttype} eq "A") {
 	$tmpstr = ($form->{l_accno}) ? "$form->{$category}{$key}{accno} - $form->{$category}{$key}{description}" : "$form->{$category}{$key}{description}";
-	$tmpstr = qq|<a href=$translink&accno=$form->{$category}{$key}{accno} target=_blank>| . $tmpstr . qq|</a>|;
+        if ($form->{accounttype} eq 'gifi'){
+	   $tmpstr = qq|<a href=$translink&gifi_accno=$form->{$category}{$key}{accno} target=_blank>| . $tmpstr . qq|</a>|;
+	} else {
+	   $tmpstr = qq|<a href=$translink&accno=$form->{$category}{$key}{accno} target=_blank>| . $tmpstr . qq|</a>|;
+	}
 	$str .= $tmpstr;
       }
       if ($form->{$category}{$key}{charttype} eq "H") {
